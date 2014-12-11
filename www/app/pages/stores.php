43e67b3845df884c21a47fabcd4daedb9b6713a1
alparf <a.l.parfenkov@gmail.com>
2014-12-11 09:06:39 +0300

    stores

---
+++ www/app/pages/stores.php
@@ -0,0 +1,88 @@
+<div class="page-body">
+    <div class="inner">
+        <div class="content">
+            <div class="content-title">Где купить</div>
+            <div class="post">
+                <div class="title">
+                    <div class="text">Яблоки</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/apple.JPG">
+                    </div>
+                    <div class="text">
+                        Отличное качество и замечательный вкус. Широкий выбор сортов.
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="title">
+                    <div class="text">Груши</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/grusha.JPG">
+                    </div>
+                    <div class="text">
+                        Плоды средней величины правильной грушевидной формы. Кожица шероховатая.
+                        Мякоть белая, сочная, сладкая, хорошего вкуса.
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="title">
+                    <div class="text">Лук</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/luk.JPG">
+                    </div>
+                    <div class="text">
+                        Лук бладает приятным полуострым вкусом и широко применяеться в кулинарии.
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="title">
+                    <div class="text">Свёкла</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/svecla.jpg">
+                    </div>
+                    <div class="text">
+                        Характеризуеся овальной или почти округлой формой корнеплода.
+                        Мякоть темно-красная (бордо), светлые кольца на разрезе почти отсутствуют.
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="title">
+                    <div class="text">Морковь</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/morkov.jpg">
+                    </div>
+                    <div class="text">
+                        Морковь отличается сочной, сладкой, нежной мякотью и небольшой сердцевиной.
+                    </div>
+                </div>
+            </div>
+             <div class="post">
+                <div class="title">
+                    <div class="text">Капуста</div>
+                </div>
+                <div class="body">
+                    <div class="prev">
+                        <img src="/template/images/capusta.jpg">
+                    </div>
+                    <div class="text">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <? get_template('right-bar'); ?>
+    </div>
+</div>
