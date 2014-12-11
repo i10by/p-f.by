2350653b02fddf65a43ffcc4e5ff41af3e5becba
alparf <a.l.parfenkov@gmail.com>
2014-12-11 09:09:15 +0300

    stores

--- www/template/header.php
+++ www/template/header.php
@@ -2,7 +2,7 @@
     <div class="img"></div>
     <div class="menu">
         <a href="/" class="<? active_is_page(""); ?>">Продукция</a>
-        <a href="/" class="<? active_is_page("stores"); ?>">Продукция</a>
+        <a href="/stores" class="<? active_is_page("stores"); ?>">Продукция</a>
         <a href="/ord" class="<? active_is_page("ord"); ?>">Оставить заявку</a>
     </div>
 </div>
