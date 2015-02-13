857c52f0ab6636ee59293b0d47a1124ac45ca251
a.l.parf <a.l.parfenkov@gmail.com>
2015-02-13 23:21:41 +0300

    modifier

--- js/gui.js
+++ js/gui.js
@@ -2,23 +2,10 @@
  * Created by a.l.parf on 13.02.2015.
  */
 function main() {
-    resizeWin();
     load('pages/About.html');
-    $(window).resize( function() {
-        resizeWin();
-    });
 }
 
 function select(item) {
     $(".menu-item").removeClass("sel");
     $("#" + item).addClass("sel");
-}
-
-function resizeWin() {
-    var winWidth = window.innerWidth;
-    var logoWidth = 298;
-    if (!(winWidth < 600)) {
-        var logoWidth = winWidth - 302;
-    }
-    $("div.logo").css('width', logoWidth);
 }
